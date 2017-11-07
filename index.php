0ffe3ec14bb7887aecddc3cc807fc4f875aec3be
Yinka Ash <developer@designsbynetguru.com>
2017-11-08 01:41:25 +0800

    Finish Ezy Landing Page

--- index.php
+++ index.php
@@ -110,13 +110,7 @@
                         </div>
                       </div>
 
-                      <div class="form-group">
-                        <div class="input-group">
-                          <div class="input-group addon">
-											    <div class="flagstrap" id="select_country" data-input-name="NewBuyer_country" data-selected-country=""></div>
-                          </div>
-                        </div>
-                      </div>
+
 
                      <div class="form-group">
                         <div class="input-group">
@@ -141,9 +135,378 @@
                         <input type="submit" name="subscribeBtn" value="SIGN ME UP &raquo;" class="btn btn-danger btn-block btn-lg">
                       </div>
                     </form>
-                    <p id="privacy"><small>We do NOT share your information with 3rd parties.</small></p>
+
                   </div>
                 </div>
+						</div>
+
+						<div class="row">
+						  <div class="col-md-12">
+								<div class="signUp" style="margin-top: -30px; margin-bottom: 30px;">
+									<h2 style="font-size: 2em; font-weight: 700">TESTIMONIALS</h2>
+								</div>
+						  </div>
+
+							<div class="col-md-12 testimony">
+
+
+								<div class="col-md-4">
+							    <div class="panel panel-success">
+							      <div class="panel-heading">
+							        <h3 class="panel-title">- Angela Gina</h3>
+							      </div>
+							      <div class="panel-body">
+											<p class="text-warning">&quot;Zero to Hero&quot; affordable Forex training made my learning experience so enjoyable.  <br><br>They held my hand through all my trades with their mentoring. Since I started with &quot;Zero to Hero&quot; I have doubled my account in 2 months while being fully employed.&quot;</p>
+							      </div>
+							      <div class="panel-footer">
+											<p class="text-right text-primary">&mdash; Jan '17</p>
+							      </div>
+							    </div>
+							  </div>
+
+
+								<div class="col-md-4">
+							    <div class="panel panel-info">
+							      <div class="panel-heading">
+							        <h3 class="panel-title">- Rowan Marais</h3>
+							      </div>
+							      <div class="panel-body">
+											<blockquote>
+												<p class="text-warning">I was always interested in forex trading. I never got into it after hearing about all the forex training scams there are out there. <br><br>Out of curiosity, I attended Zero to Hero free Saturday classes and was so impressed with their high level of experience in trading forex. I was not completely convinced until I saw their personal trading results. Those guys know exactly what they are doing.</p>
+											</blockquote>
+							      </div>
+							      <div class="panel-footer">
+											<p class="text-right text-primary">&mdash; Jan '17</p>
+							      </div>
+							    </div>
+							  </div>
+
+
+								<div class="col-md-4">
+							    <div class="panel panel-danger">
+							      <div class="panel-heading">
+							        <h3 class="panel-title">- Genevieve Erlank</h3>
+							      </div>
+							      <div class="panel-body">
+											<p class="text-warning">I love to travel. I have been to so many countries which is why I love trading forex. <br><br>No matter where in the world I am I can still make money. The markets are open 24 hours a day so you can trade anytime… Anywhere!</p>
+							      </div>
+							      <div class="panel-footer">
+											<p class="text-right text-primary">&mdash; Feb '17</p>
+							      </div>
+							    </div>
+							  </div>
+
+
+							  <div class="col-md-4">
+							    <div class="panel panel-success">
+							      <div class="panel-heading">
+							        <h3 class="panel-title">- Yinka <span>. Penang</span></h3>
+							      </div>
+							      <div class="panel-body">
+											<p class="text-warning">I am a common forex trader, trading in for a while. Actually, I am not a successful
+trader, usually lose my funds. Here I share my story to help the new traders get
+success. <br><br>
+After reading several forex education materials, I registered a standard account and
+deposited 500$ in it. Maybe you have thought out the result of this account, I lost all
+the 500$. It was a big shock for me. I intent to make money using this 500$, but the
+result was the opposite.<br><br>
+Later I joined &quot;Zero to Hero&quot; . This time the result is better. Finally, I find out the
+better way to earn in forex trading. I doubled my account in 1 month.</p>
+							      </div>
+							      <div class="panel-footer">
+											<p class="text-right text-primary">&mdash; Mar '17</p>
+							      </div>
+							    </div>
+							  </div>
+
+
+								<div class="col-md-4">
+							    <div class="panel panel-info">
+							      <div class="panel-heading">
+							        <h3 class="panel-title">- Josh <span>. Kedah</span></h3>
+							      </div>
+							      <div class="panel-body">
+											<blockquote>
+												<p class="text-warning">I really had my reservations about this system as I have always looked at trading as too
+	complicated and only for professionals... obviously I was wrong. I started off only trading
+	$250 and made a loss in the first week as I didn't follow the system exact (I have no idea
+	why I love doing things the hard way..;). After following the system step by step I made
+	$1,500 in the next month!! This has given me so much financial freedom.</p>
+											</blockquote>
+							      </div>
+							      <div class="panel-footer">
+											<p class="text-right text-primary">&mdash; Mar '17</p>
+							      </div>
+							    </div>
+							  </div>
+
+
+								<div class="col-md-4">
+							    <div class="panel panel-danger">
+							      <div class="panel-heading">
+							        <h3 class="panel-title">- Rani <span>. Labuan</span></h3>
+							      </div>
+							      <div class="panel-body">
+											<p class="text-warning">I began with $500 in, and I went down to $50 the first day. I rushed too much on the
+trades without following the trends you suggested in the system. <br><br>Then I began following
+them seriously and won, and won, and won some more the next day! ... <br><br>Thank You! I
+didn't believe this would work so fast, It just seemed too simple, you know? Now I am
+extremely confident, as I have already tripled my starting capital! It's amazing how
+effective this is."</p>
+							      </div>
+							      <div class="panel-footer">
+											<p class="text-right text-primary">&mdash; Apr '17</p>
+							      </div>
+							    </div>
+							  </div>
+
+
+
+
+								<div class="col-md-4">
+							    <div class="panel panel-primary">
+							      <div class="panel-heading">
+							        <h3 class="panel-title">- Malik <span>. Kuala Lumpur</span></h3>
+							      </div>
+							      <div class="panel-body">
+											<p class="text-warning">I loved the way things moved so fast, I just filled up the form and 5 minutes after the
+phone rang with the guy from EZY SOCIO on the other side, helping me to open the
+account. <br><br>I deposited $1,000, played it out nice and slow, watched the graphs and chose
+my battles wisely. I didn't double my money like I thought I would, but over $400 profit on
+the first day of trading isn't that bad either...</p>
+							      </div>
+							      <div class="panel-footer">
+											<p class="text-right text-primary">&mdash; June '17</p>
+							      </div>
+							    </div>
+							  </div>
+
+
+
+
+								<div class="col-md-4">
+							    <div class="panel panel-default">
+							      <div class="panel-heading">
+							        <h3 class="panel-title">- Kasim <span>. Kuantan</span></h3>
+							      </div>
+							      <div class="panel-body">
+											<p class="text-warning">By luck, I stumbled upon your site, and of course I wanted to try it out. <br><br>Deposited $500
+with EZY SOCIO opened 5 positions and won 4. Took out the $500 and still have the
+profit of $388 to trade with. <br><br>This is a great system you got there Sir, I definitely owe you
+one.</p>
+							      </div>
+							      <div class="panel-footer">
+											<p class="text-right text-primary">&mdash; Sep '17</p>
+							      </div>
+							    </div>
+							  </div>
+
+
+								<div class="col-md-4">
+							    <div class="panel panel-warning">
+							      <div class="panel-heading">
+							        <h3 class="panel-title">- Monica <span>. Labuan</span></h3>
+							      </div>
+							      <div class="panel-body">
+											<p class="text-warning">I began with $500 in, and I went down to $50 the first day. I rushed too much on the
+trades without following the trends you suggested in the system. <br><br>Then I began following
+them seriously and won, and won, and won some more the next day! ... <br><br>Thank You! I
+didn't believe this would work so fast, It just seemed too simple, you know? Now I am
+extremely confident, as I have already tripled my starting capital! It's amazing how
+effective this is."</p>
+							      </div>
+							      <div class="panel-footer">
+											<p class="text-right text-primary">&mdash; Oct '17</p>
+							      </div>
+							    </div>
+							  </div>
+
+							</div>
+						</div>
+
+						<div class="row">
+						  <div class="container">
+						    <div class="col-md-12">
+						      <div class="intro-text2">
+										<h2 style="font-weight: 700; margin-bottom: 30px;">The Best Amounts of Investments</h2>
+
+										<p>Even when you follow the system perfectly, it is normal to lose some
+	of the positions that you open, it is very unlikely to win all of the
+	trades you will do.</p>
+
+										<p>My system will enable you to win more trades then the ones that will
+be lost, which leads to you earning significantly on your account. </p>
+
+										<p>There may be a bad day where you will make several losing trades,
+which is why it is important to follow up with this strict rule of
+investment:</p>			<ul class="list-group">
+												<li class="list-unstyled">If you have $250 in your account, each open position should be $10 tops</li>
+												<li class="list-unstyled">If you have $500 in your account, each open position should be $25 tops</li>
+												<li class="list-unstyled">If you have $1,000 in your account, each open position should be $50 tops</li>
+												<li class="list-unstyled">If you have $2,000 in your account, each open position should be $100 tops</li>
+												<li class="list-unstyled">If you have $5,000 in your account, each open position should be $250 tops</li>
+										</ul>
+
+										<p>And so on: Each position you open should not represent more than
+5% of your capital.</p>
+						      </div>
+						    </div>
+
+								<div class="col-md-12">
+									<div class="faq">
+										<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+										  <div class="panel panel-default">
+										    <div class="panel-heading" role="tab" id="headingOne">
+										      <h4 class="panel-title">
+										        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+										          What is the Zero to Hero System?
+										        </a>
+										      </h4>
+										    </div>
+										    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+										      <div class="panel-body text-warning">
+														The Zero to Hero is a 100% FREE system that will pour light on
+trading with just a few simple clicks! It will show you how to earn
+money by simple trading actions!
+										      </div>
+										    </div>
+										  </div>
+										  <div class="panel panel-default">
+										    <div class="panel-heading" role="tab" id="headingTwo">
+										      <h4 class="panel-title">
+										        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+										          How much money can I make with this
+										        </a>
+										      </h4>
+										    </div>
+										    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+										      <div class="panel-body text-warning">
+														To put it in short, there's no limit. To put it more technically, it really
+depends on how much money you fund your account with. Most of
+our members fund their account with $500, and make about $1,000-
+2,000 monthly. Those who fund their account with $5,000 or over
+make over $18,500 per month!
+										      </div>
+										    </div>
+										  </div>
+										  <div class="panel panel-default">
+										    <div class="panel-heading" role="tab" id="headingThree">
+										      <h4 class="panel-title">
+										        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+										          How long does each trade last?
+										        </a>
+										      </h4>
+										    </div>
+										    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+										      <div class="panel-body text-warning">
+														Anywhere from a minute to an hour. So you will see your profits right
+away, no more waiting for long periods of time.
+										      </div>
+										    </div>
+										  </div>
+
+
+											<div class="panel panel-default">
+										    <div class="panel-heading" role="tab" id="headingFour">
+										      <h4 class="panel-title">
+										        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+										          Do I need to use my credit card when I signup?
+										        </a>
+										      </h4>
+										    </div>
+										    <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+										      <div class="panel-body text-warning">
+														<strong>NO WAY.</strong> The Zero to Hero is 100% FREE. You will NOT be asked
+for your credit card or paypal or bank information when you sign up. <br><br>
+Once you are in the members area, we will recommend a broker that
+you can exploit - EZYFX is our preferred broker and you can activate
+your account for trading instantly. For this, you'll need funds to
+deposit in order to start making profits. This is YOUR money that you
+are just depositing to trade with, and you can withdraw it at any time!
+										      </div>
+										    </div>
+										  </div>
+
+
+											<div class="panel panel-default">
+										    <div class="panel-heading" role="tab" id="headingFive">
+										      <h4 class="panel-title">
+										        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+										          Does your system work with Mac computers?
+										        </a>
+										      </h4>
+										    </div>
+										    <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+										      <div class="panel-body text-warning">
+														Yes! It is a web based application which means it can be used on
+Windows, Mac or Linux based systems! You just need a web
+browser.
+										      </div>
+										    </div>
+										  </div>
+
+
+											<div class="panel panel-default">
+										    <div class="panel-heading" role="tab" id="headingSix">
+										      <h4 class="panel-title">
+										        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+										          I don't have much free time - Is the Zero to Hero really automated?
+										        </a>
+										      </h4>
+										    </div>
+										    <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSix">
+										      <div class="panel-body text-warning">
+														<strong>Yes!</strong> You simply click in the system and it will trade for you! You can
+walk away from your computer, and enjoy your profits! You don't
+have to be anywhere near the system for it to work for you and make
+you thousands per day!
+										      </div>
+										    </div>
+										  </div>
+
+
+											<div class="panel panel-default">
+										    <div class="panel-heading" role="tab" id="headingSeven">
+										      <h4 class="panel-title">
+										        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+										          How easy is it to use the system?
+										        </a>
+										      </h4>
+										    </div>
+										    <div id="collapseSeven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSeven">
+										      <div class="panel-body text-warning">
+														VERY EASY! Once you sign up in the members area by filling in a
+simple form, the system is infront of you, will make profits! There is
+no complicated setup!
+										      </div>
+										    </div>
+										  </div>
+
+
+											<div class="panel panel-default">
+										    <div class="panel-heading" role="tab" id="headingEight">
+										      <h4 class="panel-title">
+										        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+										          What about customer support?
+										        </a>
+										      </h4>
+										    </div>
+										    <div id="collapseEight" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingEight">
+										      <div class="panel-body text-warning">
+														Our customer support team operates 7 days per week - 24 hours a
+day. <strong>We have email support but more importantly we have a
+FULL live chat system in the members area with our support
+agents ready to help you at ANY time! 2am? NO PROBLEM!</strong>
+										      </div>
+										    </div>
+										  </div>
+
+											<p id="privacy" class="text-center" style="margin-top: 30px;"><small>We do NOT share your information with 3rd parties.</small></p>
+										</div>
+
+									</div>
+								</div>
+						  </div>
 						</div>
 					</div>
 
